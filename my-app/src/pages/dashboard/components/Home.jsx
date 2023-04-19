2fdee0ba0376c5d830a6b364ce74799acad2712c
9KICKS <officialjoshua9@gmail.com>
2023-04-19 19:52:09 +0100

    "updated sneakerquiz"

--- my-app/src/pages/dashboard/components/Home.jsx
+++ my-app/src/pages/dashboard/components/Home.jsx
@@ -8,7 +8,8 @@ function Home() {
             <img src={Logo} alt="9KICKS Logo" className="logo"/>
             <h1 className="home-page-text">Welcome to the 9KICKS Sneaker Quiz</h1>
             <h2 className="home-page-text">Are you truly a sneakerhead? Let's find out</h2>
-            <button className="begin-quiz-button">Begin Quiz!</button>
+            <button className="begin-quiz-button" onClick={() => window.location.href = "/quiz"}>Begin Quiz!</button>
+            {/*<a href="/quiz" className="begin-quiz-button">Begin Quiz!</a>*/}
         </div>
     );
 }
